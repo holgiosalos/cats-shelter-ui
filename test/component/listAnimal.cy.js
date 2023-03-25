940e3d7444492e0c605b5e1f3596f43a84eb3bcd
Juan Esteban Caicedo Alzate <61593226+TheLordJuanes@users.noreply.github.com>
2023-03-25 12:26:31 -0500

    Animal list component test implemented (#3)

---
+++ test/component/listAnimal.cy.js
@@ -0,0 +1,64 @@
+import React from 'react'
+import ListAnimal from '@/pages/animal/list'
+
+describe('<ListAnimal />', () => {
+    beforeEach(() => {
+        cy.intercept(
+            'GET',
+            '/animals',
+            [
+                {
+                    "name":"Manchas",
+                    "breed":"Bengali",
+                    "gender":"Female",
+                    "isVaccinated":true,
+                    "vaccines": ["rabia","leucemia","parvovirus"]
+                }
+            ]
+        );
+    });
+
+    it('renders', () => {
+        cy.mount(<ListAnimal />)
+    })
+
+    it('Gender is displayed correctly', () => {
+        cy.mount(<ListAnimal />)
+
+        cy.get('[data-testid="Manchas-container"]')
+            .get('[name=female-icon]')
+            .should('be.visible');
+
+        cy.get('[data-testid="Manchas-container"]')
+            .get('[name=male-icon]')
+            .should('not.exist');
+    })
+
+    it('Vaccinated icon is displayed correctly', () => {
+        cy.mount(<ListAnimal />)
+
+        cy.get('[data-testid="Manchas-container"]')
+            .get('[name=health-icon]')
+            .should('be.visible');
+
+        cy.get('[data-testid="Manchas-container"]')
+            .get('[name=unhealthy-icon]')
+            .should('not.exist');
+    })
+
+    it('Animal data is displayed correctly', () => {
+        cy.mount(<ListAnimal />)
+
+        cy.get('[data-testid="Manchas-container"]')
+            .get('[name=name-cat]')
+            .should('be.visible');
+
+        cy.get('[data-testid="Manchas-container"]')
+            .get('[name=breed-cat]')
+            .should('be.visible');
+
+        cy.get('[data-testid="Manchas-container"]')
+            .get('[name=vaccines-list-cat]')
+            .should('be.visible');
+    })
+})
