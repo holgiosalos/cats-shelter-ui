53473d6290b6bcecaf21fb65e7036d15b6d3a2b2
Yimar T <42156268+Yimar22@users.noreply.github.com>
2023-05-17 22:34:55 -0500

    Revert "Revert "implemented unit test for list, delete, getAnimal, updateAnimal" (#11)" (#12)
    This reverts commit 5b202ef5292ea1efd1d0ff59dc4be2de8aa83978.

--- test/unit/AnimalsController.test.js
+++ test/unit/AnimalsController.test.js
@@ -31,6 +31,128 @@ describe('Animal Controller Unit Tests', () => {
         const actualResponse = await AnimalController.register(animalToRegister);
 
         expect(actualResponse.status).to.be.eql(201);
-        expect(actualResponse.data).to.be.eql(animalToRegister);
+        expect(actualResponse.data).to.be.eql(animalToRegister);git
     })
+
+    it('Test List Animals', async () => {
+        // Arrange
+        const listToGet = [
+            {
+                name: "manchas",
+                breed: "Bengali",
+                gender: "Female",
+                vaccinated: true
+            },
+            {
+                name: "Luna",
+                breed: "Maine Coon",
+                gender: "Female",
+                vaccinated: false
+            }
+        ];
+
+        moxios.wait(() => {
+            const request = moxios.requests.mostRecent();
+            request.respondWith({
+                status: 200,
+                response: listToGet,
+            });
+        });
+
+        // Act
+        const actualResponse = await AnimalController.list();
+
+        // Assert
+        expect(actualResponse.status).to.be.eql(200);
+        expect(actualResponse.data).to.be.eql(listToGet);
+        expect(actualResponse.data).to.have.length(2);
+    });
+
+    it('Test Delete Animal', async () => {
+        // Arrange
+        const animal = {
+            name: "manchas",
+            breed: "Bengali",
+            gender: "Female",
+            vaccinated: true
+        }
+
+        moxios.wait(() => {
+            const request = moxios.requests.mostRecent();
+            request.respondWith({
+                status: 200,
+            });
+        });
+
+        // Act
+        const actualResponse = await AnimalController.delete(animalToDelete.name);
+
+        // Assert
+        expect(actualResponse.status).to.be.eql(200);
+    });
+
+    it('Test get animal', async () => {
+        const animal = {
+            name: "manchas",
+            breed: "Bengali",
+            gender: "Female",
+            vaccinated: true
+        }
+
+        moxios.wait(() => {
+            const request = moxios.requests.mostRecent();
+            request.respondWith({
+                status: 200,
+                response: animal,
+            });
+        });
+
+        const actualResponse = await AnimalController.getAnimal(animal.name);
+        expect(actualResponse.status).to.be.eql(200);
+        expect(actualResponse.data).to.be.eql(animal);
+    });
+
+    it('Test update animal', async () => {
+        const animal = {
+            name: "manchas",
+            breed: "Bengali",
+            gender: "Female",
+            vaccinated: true
+        }
+
+        moxios.wait(() => {
+            const request = moxios.requests.mostRecent();
+            request.respondWith({
+                status: 200,
+                response: animal,
+            });
+        });
+
+        const actualResponse = await AnimalController.updateAnimal(animal.name);
+        expect(actualResponse.status).to.be.eql(200);
+        expect(actualResponse.data).to.be.eql(animal);
+    });
+
+    it('Test update animal', async () => {
+        // Arrange
+        const animal = {
+            name: "manchas",
+            breed: "Bengali",
+            gender: "Female",
+            vaccinated: true
+        }
+
+        moxios.wait(() => {
+            const request = moxios.requests.mostRecent();
+            request.respondWith({
+                status: 200,
+                response: animal,
+            });
+        });
+        // Act
+        const actualResponse = await AnimalController.updateAnimal(animal.name);
+        // Assert
+        expect(actualResponse.status).to.be.eql(200);
+        expect(actualResponse.data).to.be.eql(animal);
+    });
 })
